00d25b066a1bf3c24a19958b1baeec72583dd4a2
Anthony Stephen Payne <anthonypayne942@gmail.com>
2018-05-10 21:17:13 -0700

    server running and sql almost working

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("app/public"));
+
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
