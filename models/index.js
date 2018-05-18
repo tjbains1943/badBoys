7d129348419c1bf6e4c552dc0a370d9a3cb6dc23
Nick Peel <nickpeel@Nicks-MBP.localdomain>
2018-05-17 19:30:39 -0700

    asdfa

--- models/index.js
+++ models/index.js
@@ -8,6 +8,7 @@ var env = process.env.JAWSDB_URL || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
+console.log(env, config)
 // if (config.use_env_variable) {
 //   var sequelize = new Sequelize(process.env[config.use_env_variable]);
 // } else {
