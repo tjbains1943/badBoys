da73175cd7fbdd036879ef4ec34f1995f8b42f5c
Nick Peel <nickpeel@Nicks-MBP.localdomain>
2018-05-17 19:32:33 -0700

    config for jawsdb

--- models/index.js
+++ models/index.js
@@ -8,13 +8,14 @@ var env = process.env.JAWSDB_URL || "development";
 var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
-console.log(env, config)
-// if (config.use_env_variable) {
-//   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-// } else {
-//   var sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
-var sequelize = new Sequelize(process.env.JAWSDB_URL)
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 
 fs
   .readdirSync(__dirname)
